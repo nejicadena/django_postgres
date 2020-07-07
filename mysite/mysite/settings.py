bdabe1813b7dd56e5f0e6c07f9bc4079d3361964
Eduardo <nejicadena@hotmail.com>
2020-07-07 13:54:59 -0500

    add settings postgres

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sammy',
+        'USER': 'postgres',
+        'PASSWORD':'lalo2394',
+        'HOST': 'localhost',
+        'PORT': '5432'
+
     }
 }
 
